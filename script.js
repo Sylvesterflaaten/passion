36ccbbb46008393c3098c531c655a82094f7c2f1
Sylvesterflaaten <sylv1791@stud.kea.dk>
2020-02-27 23:45:23 +0100

    last commit

--- script.js
+++ script.js
@@ -1,38 +1,43 @@
-        const endpoint = "https://spreadsheets.google.com/feeds/list/1qgHX1DA5a0seGp-zUEolxOHpkMEW9Kmvjbh9SJ8bkc4/od6/public/values?alt=json";
+        /*LINK TIL JSON FILERNE FRA SHEET, MED ALT INFO*/
+        var endpoint = "https://spreadsheets.google.com/feeds/list/1qgHX1DA5a0seGp-zUEolxOHpkMEW9Kmvjbh9SJ8bkc4/od6/public/values?alt=json";
 
+        /*VARIABLER*/
         let produkter = [];
         let filterKnap = document.querySelectorAll(".filter");
         let filter = "alle";
 
         document.addEventListener("DOMContentLoaded", start);
 
-
+        /*NÅR SIDEN LOADES, LOADES STARTFUNKTIONERNE I DENNE FUNKTION*/
         function start() {
             hentData();
+
             filterKnap.forEach(knap => {
                 knap.addEventListener("click", filtrering);
             });
         }
 
+        /*FETCHER INFO FRA JSON, SOM INDSÆTTES I TEMPLATEN*/
         async function hentData() {
             const response = await fetch(endpoint);
             produkter = await response.json();
             console.log(produkter);
             visProdukter();
+
         }
 
-
+        /*FILTRERE SERIERNE PÅ TV-SHOWS MELLEM ALLE GENRES*/
         function filtrering() {
             console.log("filtrering");
-            filter = this.dataset.kategori; // sæt variabel "filter" til aktuel værdi
-            document.querySelector(".valgt").classList.remove("valgt"); // fjern klassen valgt fra aktuel knap
-            this.classList.add("valgt") // marker den nyvalgte knap
-            visProdukter(); // kald funktionen vis igen med nyt filter
+            filter = this.dataset.kategori; // sætter variabel "filter" til aktuel værdi
+            document.querySelector(".valgt").classList.remove("valgt"); // fjerner klassen valgt fra aktuel knap
+            this.classList.add("valgt") // markere den nyvalgte knap
+            visProdukter(); // kalder funktionen vis igen med nyt filter
         }
 
-
-
+        /*INDSÆTTER INFOEN FRA JSON PÅ SIDEN*/
         function visProdukter() {
+
             let container = document.querySelector("#container");
             let produktTemplate = document.querySelector("template");
             let article = document.querySelector("article");
@@ -45,11 +50,10 @@
                     klon.querySelector("h2").textContent = produkt.gsx$navn.$t;
                     klon.querySelector("img").src = `imgs/${produkt.gsx$billede.$t}.jpg`;
                     klon.querySelector(".kort").textContent += produkt.gsx$kort.$t;
-                    /*                    klon.querySelector(".genre").textContent += `${produkt.gsx$genre.$t}`;*/
 
                     klon.querySelector(".produkt").addEventListener("click", () => {
                         location.href = `detalje.html?id=${produkt.gsx$id.$t}`;
-                        //visDetalje(produkt);
+
                     });
                     container.appendChild(klon);
                 }
